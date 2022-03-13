163d5ef6a2754dcb708886cd1c589f46ffcf35bb
JunKoding <wnsrb0407@daum.net>
2022-03-13 20:55:54 +0900

    2022.03.13 commit

--- MusicPlayer/ViewController.swift
+++ MusicPlayer/ViewController.swift
@@ -8,7 +8,14 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    // Mark: IBOutlets
+    @IBOutlet var playPauseButton: UIButton!
+    @IBOutlet var timeRabel: UILabel!
+    @IBOutlet var progressSlider: UISlider!
 
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
